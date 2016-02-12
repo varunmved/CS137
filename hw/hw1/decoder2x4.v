ef09684bb5d74e10ac77d874461a3d8d3e51ae6e
Varun Ved <varunmved@gmail.com>
2016-02-11 19:46:15 -0800

    what the fuck is with these z's

--- hw/hw1/decoder2x4.v
+++ hw/hw1/decoder2x4.v
@@ -8,14 +8,18 @@ module DecoderMod(s0,s1,o0,o1,o2,o3);
     input s0;
     input s1;
     output o0,o1,o2,o3;
+    
+    //add wires
+    //wire s1_inv,s0_inv,and0,and1,and2,and3;
+    reg s1_inv,s0_inv,and0,and1,and2,and3;
 
-    //assign outputs
-    assign o0 = (~s0) & (~s1);
-    assign o1 = (~s0) & (s1);
-    assign o2 = (s1) & (~s0);
-    assign o3 = (s1) & (s0);
 
-endmodule
+    assign o0 = (~s0) && (~s1);
+    assign o1 = (s0) && (~s1);
+    assign o2 = (s1) && (~s0);
+    assign o3 = (s1) && (s0);
+    
+    endmodule
 
 module TestMod;
     //declare flip flop and wires
@@ -27,18 +31,22 @@ module TestMod;
     
     initial begin
             $monitor("%0d\t%b\t%b\t%b\t%b\t%b\t%b", $time, s0, s1, o0, o1, o2, o3);
-            $display("Time\ts\to0\to1");
+            $display("Time\ts0\ts1\to0\to1\to2\to3");
             $display("--------------------------");
     end
 
     initial begin
       s0 = 0;       // initially s is 0
-      #1;          // wait 1 simulation time unit/cycle
-      s1 = 1;       // change s to 1
+      s1 = 0;       // change s to 1
       #1;          // wait 1 simulation time unit/cycle
       s0 = 0;       // change s to 0
-      #1;          // wait 1 simulation time unit/cycle
       s1 = 1;       // change s to 1
-   end
+      #1
+      s0 = 1;       // change s to 0
+      s1 = 0;
+      #1
+      s0 = 1;       // change s to 0
+      s1 = 1; 
+  end
    endmodule
     
